51a45dbad5cb0aa983ce9bbd8cc97e0992d4ac58
Hmazah <alhamza2012@gmail.com>
2024-07-30 12:00:00 +0300

    add  server.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 
 // import prisma from '@/lib/db';
+import { useTRPC } from '@/trpc/client'
 import React from 'react'
 
 
-const page = async () => {
+
+const page = () => {
   // const posts = await prisma.post.findMany();
+  const trpc =useTRPC();
+  trpc.creatAI.queryOptions({text:"Hello"});
+  
   return (
     <div  className='font-bold  text-rose-500'>
        {/* {JSON.stringify(posts,null,2)} */}
