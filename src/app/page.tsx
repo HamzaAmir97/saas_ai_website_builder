42a7320a78dedfe72813d6dafe43e173e6f763e0
Hmazah <alhamza2012@gmail.com>
2024-08-30 12:00:00 +0300

    calling inggest using trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
+'use client';
 
+import { Button } from "@/components/ui/button"
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
 
+ 
 const page = () => {
-
+  const trpc = useTRPC();
+  const invok = useMutation(trpc.invoke.mutationOptions({}));
   
 
   return (
-    <div  className='font-bold  text-rose-500'>
-      
-      hello world
+    <div  className='font-bold p-4  text-rose-500'>
+    <Button  onClick={()=>invok.mutate({text:'hamza'})}>
+   invok baground
+
+    </Button>
     </div>
   )
 }
