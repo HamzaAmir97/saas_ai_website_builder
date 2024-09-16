7f6dc3f14625bf13ce61b6d4a4dd85a6ffc394d8
Hmazah <alhamza2012@gmail.com>
2024-09-16 12:00:00 +0300

    start building code floder tree

--- src/components/tree-view.tsx
+++ src/components/tree-view.tsx
@@ -10,6 +10,8 @@ interface TreeViewProps {
 
 import React from 'react'
 import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider } from "./ui/sidebar";
+import { FileIcon } from "lucide-react";
+import { Collapsible } from "@radix-ui/react-collapsible";
 
 const TreeView = ({data,
     value,
@@ -70,16 +72,35 @@ const Tree = ({item,
 
     const [name ,...items] = Array.isArray(item) ? item : [item];
     const currenPath= parentPath ? `${parentPath}/name` : name;
-     
+ 
+    if(!items.length){
     const isSelected = SelectedValue === currenPath;
    
     return (
     <SidebarMenuButton
     isActive={isSelected}
-    className="data-[active=true]:bg-transparent">
-        
+    className="data-[active=true]:bg-transparent"
+    onClick={(()=>{})}>
+         <FileIcon/>
+         <span className="trancate">
+
+            {name}
+         </span>
     </SidebarMenuButton>
   )
 }
+
+    //its a folder
+    
+    return (
+        <SidebarMenuItem>
+         <Collapsible>
+         
+         
+         </Collapsible>
+        </SidebarMenuItem>
+      )
+    
+};
 
 export default Tree
