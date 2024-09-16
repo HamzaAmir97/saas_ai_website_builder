55540867ed5b53235d3388a0a5c4c98d2038bb9c
Hmazah <alhamza2012@gmail.com>
2024-09-16 12:00:00 +0300

    ad tree items and utiles

--- src/components/tree-view.tsx
+++ src/components/tree-view.tsx
@@ -9,13 +9,77 @@ interface TreeViewProps {
 }
 
 import React from 'react'
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider } from "./ui/sidebar";
 
 const TreeView = ({data,
     value,
    onSelect}:TreeViewProps) => {
   return (
-    <p>{JSON.stringify(data)}</p>
+    <SidebarProvider>
+      <Sidebar>
+
+       <SidebarContent>
+
+          <SidebarGroup>
+
+    <SidebarGroupContent>
+
+
+
+    <SidebarMenu>
+   
+   <SidebarMenuItem>
+
+
+   </SidebarMenuItem>
+
+
+    </SidebarMenu>
+
+
+    </SidebarGroupContent>
+
+
+          </SidebarGroup>
+
+
+       </SidebarContent>
+
+
+      </Sidebar>
+
+
+    </SidebarProvider>
+  )
+};
+
+interface   TreeProps {
+   
+    item : TreeItem,
+    SelectedValue ? : string |null,
+    onSelect?: (value :string )=> void,
+    parentPath : string ,
+};
+
+
+
+const Tree = ({item, 
+    SelectedValue,
+    onSelect,
+   parentPath,}:TreeProps) => {
+
+    const [name ,...items] = Array.isArray(item) ? item : [item];
+    const currenPath= parentPath ? `${parentPath}/name` : name;
+     
+    const isSelected = SelectedValue === currenPath;
+   
+    return (
+    <SidebarMenuButton
+    isActive={isSelected}
+    className="data-[active=true]:bg-transparent">
+        
+    </SidebarMenuButton>
   )
 }
 
-export default TreeView
+export default Tree
