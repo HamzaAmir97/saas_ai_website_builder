168cd7556460db9181047b3e3e83699e47722b23
Hmazah <alhamza2012@gmail.com>
2024-10-16 12:00:00 +0300

    start building home page

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -11,7 +11,8 @@ const layout = ({children}:props) => {
   return (
     <div>
     <main className=' flex flex-col min-h-screen max-h-screen'>
-      <div className="absolute insect-0 -z-10 h-full bg-background dark:bg-[radial-gradient(#393e4a_1px,transparent_px)]" />
+      <div className="absolute insect-0 -z-10 h-full bg-background 
+      dark:bg-[radial-gradient(#393e4a_1px,transparent_px)] bg-[radial-gradient(#daadd2,transparent_px)] [background-size:16px_16px]" />
 
       
       <div className='flex-1 flex flex-col px-4 pb-4'>
