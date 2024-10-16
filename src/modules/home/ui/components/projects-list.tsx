31730baad5ee37b0300fea4209fa51ac8dbfd75e
Hmazah <alhamza2012@gmail.com>
2024-10-16 12:00:00 +0300

    add prjects templets

--- src/modules/home/ui/components/projects-list.tsx
+++ src/modules/home/ui/components/projects-list.tsx
@@ -1,6 +1,10 @@
 'use client';
 import { useQuery } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import { formatDistanceToNow } from "date-fns";
 
 export const ProjectsList = () => {
     const trpc = useTRPC();
@@ -13,6 +17,44 @@ export const ProjectsList = () => {
 
            projects
            </h2>
+           <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
+  {projects?.length === 0 && (
+    <div className="col-span-full muted-text-center">
+      <p className="text-sm text-muted-foreground">
+        No projects found
+      </p>
+    </div>
+  )}
+  {projects?.map((project) => (
+    <Button
+      key={project.id}
+      variant="outline"
+      className="font-normal h-auto justify-start w-full text-start p-4"
+    >
+     <Link href={`/projects/${project.id}`}>
+  <div className="flex items-center gap-x-4">
+    <Image
+      src="/logo.svg"
+      alt="Codey"
+      width={32}
+      height={32}
+      className="object-contain"
+    />
+    <div className="flex flex-col">
+      <h3 className="">
+        {project.name}
+      </h3>
+      <p className="text-sm text-muted-foreground">
+  {formatDistanceToNow(project.updatedAt, {
+    addSuffix: true,
+  })}
+</p>
+    </div>
+  </div>
+</Link>
+    </Button>
+  ))}
+</div>
         </div>
     );
 };
