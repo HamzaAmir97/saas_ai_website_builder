b8548f25d42de554c2195ae0ac67361aa4742f3e
Hmazah <alhamza2012@gmail.com>
2024-11-16 12:00:00 +0200

    add clerk them package

--- src/components/user-control.tsx
+++ src/components/user-control.tsx
@@ -2,7 +2,7 @@
 
 import { useCurrentTheme } from "@/hooks/use-current-theme";
 import { UserButton } from "@clerk/nextjs";
-
+import {dark} from "@clerk/themes";
 interface Props {
   showName?: boolean;
 }
@@ -19,6 +19,7 @@ export const UserControl = ({ showName }: Props) => {
           userButtonAvatarBox: "rounded-md! size-8!",
           userButtonTrigger: "rounded-md!"
         },
+        baseTheme:currentTheme==="dark"? dark :undefined,
       }}
     />
   );
