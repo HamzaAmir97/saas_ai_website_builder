ab82dc0e49c1541dc6c12b0ad15cc6ae2796f00f
Hmazah <alhamza2012@gmail.com>
2024-12-16 12:00:00 +0200

    add usage  functions

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,10 +2,11 @@
 import { projectsRouter } from '@/modules/projects/server/proceduers';
 import {  createTRPCRouter } from '../init';
 import { messagesRouter } from '@/modules/messages/server/proceduers';
+import { usageRouter } from '@/modules/usage/server/procedure';
 export const appRouter = createTRPCRouter({
+    usage: usageRouter,
     messages : messagesRouter,
     projects : projectsRouter,
-
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
