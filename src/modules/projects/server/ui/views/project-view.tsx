ce806fc39e87a8606bb0bc888acaa4b317750e44
Hmazah <alhamza2012@gmail.com>
2025-01-16 12:00:00 +0200

    solv some ui issue

--- src/modules/projects/server/ui/views/project-view.tsx
+++ src/modules/projects/server/ui/views/project-view.tsx
@@ -19,6 +19,7 @@ import { UserControl } from "@/components/user-control";
 import { useAuth } from "@clerk/nextjs";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "@/app/error";
+import { cn } from "@/lib/utils";
 
 interface props {
     projectId: string,
@@ -26,6 +27,9 @@ interface props {
 
 
 export const ProjectView = ({ projectId }: props) => {
+
+    const [buttonEnabled , setButtonEnabled]=useState(false);
+
     const [activeFragment, setactiveFragment] = useState<Fragment | null>(null);
     const [tabState, setTabState] = useState<"preview" | "code">("preview");
     const { has } = useAuth();
@@ -69,12 +73,16 @@ export const ProjectView = ({ projectId }: props) => {
                         {/* Header section: triggers + buttons */}
                         <div className="w-full flex items-center p-2 border-b gap-x-2">
                             <TabsList className="h-8 p-0 border rounded-md">
-                                <TabsTrigger value="preview" className="rounded-md ">
-                                  <div className="flex  justify-between"><EyeIcon /> <span>Demo</span></div> 
+                                <TabsTrigger value="preview" className="rounded-md  "> 
+                                  <Button 
+                                  variant={"ghost"}
+                                   className={cn("flex  justify-between")}
+                                   onClick={()=>{setButtonEnabled(!buttonEnabled);}}
+                                   ><EyeIcon  /> <span>Demo</span> </Button>
                                 </TabsTrigger>
                                
                                 <TabsTrigger value="code" className="rounded-md ">
-                                <div className="flex  justify-between">  <Code2Icon /> <span>Code</span></div>
+                                 <Button variant={"ghost"} className="flex  justify-between "> <Code2Icon /> <span>Code</span></Button>
                                 </TabsTrigger>
 
                             </TabsList>
