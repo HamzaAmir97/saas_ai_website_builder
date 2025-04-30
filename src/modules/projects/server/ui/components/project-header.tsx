d954153aaa93f6bee1c9e7ebb22725c8326425c7
Hmazah <alhamza2012@gmail.com>
2025-04-30 12:00:00 +0300

    start with project header comp

--- src/modules/projects/server/ui/components/project-header.tsx
+++ src/modules/projects/server/ui/components/project-header.tsx
@@ -1,51 +1,89 @@
 
 import { Button } from '@/components/ui/button';
+import { DropdownMenuSeparator, DropdownMenuSub } from '@/components/ui/dropdown-menu';
 import { useTRPC } from '@/trpc/client';
-import { DropdownMenu, DropdownMenuItem, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuPortal, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuSubContent, DropdownMenuSubTrigger, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
 import { useSuspenseQuery } from '@tanstack/react-query';
-import { ChevronDownIcon } from 'lucide-react';
+import { ChevronDownIcon, ChevronLeftIcon, SunMediumIcon } from 'lucide-react';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
 interface props {
     projectId: string,
 };
 
-const ProjectHeader = ({ projectId}:props) => {
+const ProjectHeader = ({ projectId }: props) => {
     const trpc = useTRPC();
-    const { data: project} = useSuspenseQuery(
-        trpc.projects.getOne.queryOptions({id: projectId })
+    const { data: project } = useSuspenseQuery(
+        trpc.projects.getOne.queryOptions({ id: projectId })
     );
 
-  return (
-   <header  className=' p-2 flex justify-between items-center border-b'>
-      
-      <DropdownMenu>
-       <DropdownMenuTrigger asChild>
-         <Button
-         variant={"ghost"} 
-         size={"sm"}
-         className="focus-visible:ring-0 hover:bg-transparent hover:opacity-75 
+    return (
+        <header className=' p-2 flex justify-between items-center border-b'>
+
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button
+                        variant={"ghost"}
+                        size={"sm"}
+                        className="focus-visible:ring-0 hover:bg-transparent hover:opacity-75 
             transition-opacity pl-2!"
-         >
+                    >
 
-         <Image 
-          src={"/logo.svg"}
-          alt='codey'
-          width={18}
-          height={18}
-         />
-         </Button>
-         <span className='text-sm font-medium'>{project.name}</span>
-          <ChevronDownIcon/>
-       </DropdownMenuTrigger>
+                        <Image
+                            src={"/logo.svg"}
+                            alt='codey'
+                            width={18}
+                            height={18}
+                        />
+                    </Button>
+                    <span className='text-sm font-medium'>{project.name}</span>
+                    <ChevronDownIcon />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent side='bottom' align='start'>
+                   <DropdownMenuItem>
+                     <Link 
+                       href={"/"}
+                       >
+                      <ChevronLeftIcon/>
+                      <span>Go to dashboard</span>
+                     </Link>
 
 
-      </DropdownMenu>
+                   </DropdownMenuItem>
+                   <DropdownMenuSeparator/>
+
+                <DropdownMenuSub>
+
+                     <DropdownMenuSubTrigger className='gap-2'>
+                        <SunMediumIcon className='size-4 text-muted-foreground' />
+                        <span>Appearnce</span>
+                     </DropdownMenuSubTrigger>
+                     <DropdownMenuPortal>
+                         <DropdownMenuSubContent>
+
+                            <DropdownMenuRadioGroup value='Light' onValueChange={()=>{}}>
+                              <DropdownMenuRadioItem value='Light'>
+                                <span>Light</span>
+                              </DropdownMenuRadioItem>
+                              <DropdownMenuRadioItem value='Dark'>
+                                <span>Dark</span>
+                              </DropdownMenuRadioItem>
+                              <DropdownMenuRadioItem value='System'>
+                                <span>System</span>
+                              </DropdownMenuRadioItem>
+                            </DropdownMenuRadioGroup>
+                         </DropdownMenuSubContent>
+                     </DropdownMenuPortal>
+                </DropdownMenuSub>
+                </DropdownMenuContent>
+
+            </DropdownMenu>
 
 
-   </header>
-  )
+        </header>
+    )
 }
 
-export default ProjectHeader ;
+export default ProjectHeader;
