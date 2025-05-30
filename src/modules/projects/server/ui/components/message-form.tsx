a6cb085197465fab2a73b6d15a4d24b28a5627cf
Hmazah <alhamza2012@gmail.com>
2025-05-30 12:00:00 +0300

    build message-form

---
+++ src/modules/projects/server/ui/components/message-form.tsx
@@ -0,0 +1,15 @@
+interface props {
+    projectId: string,
+
+
+};
+
+import React from 'react'
+
+const messageForm = ({ projectId }: props) => {
+  return (
+    <div>message-form</div>
+  )
+}
+
+export default messageForm 
