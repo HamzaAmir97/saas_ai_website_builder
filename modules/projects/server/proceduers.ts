850e1e620fb0424687db84ba41f6f357beee36ec
Hmazah <alhamza2012@gmail.com>
2025-07-04 13:58:34 +0300

    push to main

---
+++ modules/projects/server/proceduers.ts
@@ -0,0 +1,84 @@
+import { inngest } from "@/inngest/client";
+import prisma from "@/lib/db";
+import {generateSlug} from "random-word-slugs"
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { z } from "zod";
+import { TRPCError } from "@trpc/server";
+
+
+
+export const projectsRouter = createTRPCRouter({
+  getOne : baseProcedure
+   .input(z.object({
+     id:z.string().min(1, {message:"Id is requierd"}),
+
+   }))
+  .query(async ({input})=>{
+    const existingprojects= await prisma.project.findUnique({
+     where:{
+        id :input.id,
+      },
+    });
+     
+      if (!existingprojects){
+         throw new TRPCError({code:"NOT_FOUND",message: "Project not found"})
+      }
+       return existingprojects;
+  }),
+  
+  getMany : baseProcedure
+  .query(async ()=>{
+    const projects= await prisma.project.findMany({
+      orderBy:{
+        updatedAt :"desc",
+      },
+    });
+
+       return projects;
+  }),
+  
+
+
+  create : baseProcedure
+     .input(
+        z.object({
+            value : z.string().min(1,{message: "Value is required"})
+            .max(1000,{message : "Value is too  long"}),
+           
+        })
+     )
+     .mutation(async ({input})=>{
+      const createdProject = await prisma.project.create({
+        data : {
+          name : generateSlug(2,{
+           format: "kebab",
+          }),
+          messages :{
+            create:{
+            content : input.value,
+            role : "USER",
+            type : "RESULT",
+
+          }
+        }
+        
+      }
+     
+
+        });
+
+
+
+       await inngest.send({
+        name: 'codeAgentFunction/run',
+          data : {
+
+           value : input.value,
+           projectId : createdProject.id,
+        }
+
+      })
+     return createdProject
+     }),
+
+});
---
+++ modules/projects/server/ui/components/message-loading.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react'
+
+const ShimerMessages = () => {
+    const messages = [
+        "Thinking...",
+        "Loading ...",
+        "Just a sec...",
+        "Fetching magic...",
+        "Spinning up some thoughts...",
+        "Unpacking ideas...",
+        "Hold tight, good things take time...",
+        "Cooking up a response...",
+        "Crunching some data...",
+        "Almost there..."
+      ];
+    const [currentMessageIndex,setcurrentMessageIndex]= useState(0);
+    useEffect(()=>{
+    const interval = setInterval(()=>{
+     setcurrentMessageIndex((prev)=>(prev+1) % messages.length);
+
+    },2000)
+
+   return()=> clearInterval(interval);
+
+    },[]);
+  
+    return (
+    <div className='flex items-center gap-2'>
+        <span className='text-base text-muted-foreground animate-pulse'>
+          {messages[currentMessageIndex]}
+        </span></div>
+  )
+}
+
+export const MessageLoading =()=>{
+  
+    return(
+
+        <div className=' flex flex-col group px-2 pb-4'>
+            <div className='flex items-center gap-2 pt-2 mb-2'>
+               <Image 
+               
+               src={"/logo.svg"}
+               alt ="codey"
+               width={18}
+               height={18}
+               className='shrink-0S'
+               />
+              <span className='text-sm font-medium'> Codey</span>
+            </div>
+            <div className=' pt-8.5 flex flex-col gap-y-4'>
+
+                       <ShimerMessages/>
+            </div>
+        </div>
+    )
+
+
+}
